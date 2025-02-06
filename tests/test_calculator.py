981d06df05e37300277545d0dc7852c0d0a6d1f8
Krisa29 <sathvika.ganni@gmail.com>
2025-02-06 14:44:29 -0500

    Fixed errors in test_calculator to run pytest --pylint

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,14 +2,20 @@
 
 from calculator import Calculator
 
-def test_addition(): # Addition function    
+def test_addition(): 
+    ''' Addition function '''  
     assert Calculator.add(3,4) == 7
 
-def test_subtraction(): # Subtraction function    
+def test_subtraction():
+    ''' Subtraction function '''    
     assert Calculator.subtract(3,2) == 1
 
-def test_multiply(): # Multiplication function    
+def test_multiply(): 
+    ''' Multiplication function '''    
     assert Calculator.multiply(3,3) == 9
 
-def test_divide():  # Division function    
+def test_divide():  
+    ''' Division function '''    
     assert Calculator.divide(4,2) == 2
+
+#End
