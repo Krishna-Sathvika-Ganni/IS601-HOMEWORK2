079949220d65d4c0945c0d620d806a6b5867bffa
Krisa29 <sathvika.ganni@gmail.com>
2025-02-06 14:20:37 -0500

    Defined Addition, Subtraction, Multiplication and Division functions in the operations file

---
+++ calculator/operations.py
@@ -0,0 +1,15 @@
+'''Addition, Subtraction, Multiplication and Division functions defined'''
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+# End 
